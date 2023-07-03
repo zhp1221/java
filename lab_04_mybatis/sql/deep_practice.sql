dffff0013fbb4557520c92950affec955566d7ec
zhanghepeng <zhanghp1221@126.com>
2023-07-03 23:20:55 +0800

    feat:sql文件数据

--- lab_04_mybatis/sql/deep_practice.sql
+++ lab_04_mybatis/sql/deep_practice.sql
@@ -11,38 +11,42 @@
  Target Server Version : 80033
  File Encoding         : 65001
 
- Date: 02/07/2023 23:00:48
+ Date: 03/07/2023 23:09:40
 */
 
 SET NAMES utf8mb4;
-SET
-FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for dept
 -- ----------------------------
 DROP TABLE IF EXISTS `dept`;
-CREATE TABLE `dept`
-(
-    `DEPTNO` int NOT NULL,
-    `DNAME`  varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-    `LOC`    varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-    PRIMARY KEY (`DEPTNO`) USING BTREE
+CREATE TABLE `dept`  (
+  `DEPTNO` int NOT NULL,
+  `DNAME` varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `LOC` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`DEPTNO`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (10, 'ACCOUNTING', 'NEW YORK');
+INSERT INTO `dept` VALUES (20, 'RESEARCH', 'DALLAS');
+INSERT INTO `dept` VALUES (30, 'SALES', 'CHICAGO');
+INSERT INTO `dept` VALUES (40, 'OPERATIONS', 'BOSTON');
 
 -- ----------------------------
 -- Table structure for emp
 -- ----------------------------
 DROP TABLE IF EXISTS `emp`;
-CREATE TABLE `emp`
-(
-    `EMPNO`    int NOT NULL,
-    `ENAME`    varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-    `JOB`      varchar(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-    `MGR`      int NULL DEFAULT NULL,
-    `HIREDATE` date NULL DEFAULT NULL,
-    `SAL`      double(7, 2
-) NULL DEFAULT NULL,
+CREATE TABLE `emp`  (
+  `EMPNO` int NOT NULL,
+  `ENAME` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `JOB` varchar(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `MGR` int NULL DEFAULT NULL,
+  `HIREDATE` date NULL DEFAULT NULL,
+  `SAL` double(7, 2) NULL DEFAULT NULL,
   `COMM` double(7, 2) NULL DEFAULT NULL,
   `DEPTNO` int NULL DEFAULT NULL,
   PRIMARY KEY (`EMPNO`) USING BTREE,
@@ -50,43 +54,77 @@ CREATE TABLE `emp`
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
+-- Records of emp
+-- ----------------------------
+INSERT INTO `emp` VALUES (7369, 'SMITH', 'CLERK', 7902, '1980-12-17', 800.00, NULL, 20);
+INSERT INTO `emp` VALUES (7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600.00, 300.00, 30);
+INSERT INTO `emp` VALUES (7521, 'WARD', 'SALESMAN', 7698, '1981-02-22', 1250.00, 500.00, 30);
+INSERT INTO `emp` VALUES (7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975.00, NULL, 20);
+INSERT INTO `emp` VALUES (7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250.00, 1400.00, 30);
+INSERT INTO `emp` VALUES (7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850.00, NULL, 30);
+INSERT INTO `emp` VALUES (7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450.00, NULL, 10);
+INSERT INTO `emp` VALUES (7788, 'SCOTT', 'ANALYST', 7566, '1987-04-19', 3000.00, NULL, 20);
+INSERT INTO `emp` VALUES (7839, 'KING', 'PRESIDENT', NULL, '1981-11-17', 5000.00, NULL, 10);
+INSERT INTO `emp` VALUES (7844, 'TURNER', 'SALESMAN', 7698, '1981-09-08', 1500.00, 0.00, 30);
+INSERT INTO `emp` VALUES (7876, 'ADAMS', 'CLERK', 7788, '1987-05-23', 1100.00, NULL, 20);
+INSERT INTO `emp` VALUES (7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950.00, NULL, 30);
+INSERT INTO `emp` VALUES (7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000.00, NULL, 20);
+INSERT INTO `emp` VALUES (7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300.00, NULL, 10);
+
+-- ----------------------------
 -- Table structure for project
 -- ----------------------------
 DROP TABLE IF EXISTS `project`;
-CREATE TABLE `project`
-(
-    `pid`   int                                                          NOT NULL AUTO_INCREMENT,
-    `pname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    `money` int NULL DEFAULT NULL,
-    PRIMARY KEY (`pid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+CREATE TABLE `project`  (
+  `pid` int NOT NULL AUTO_INCREMENT,
+  `pname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `money` int NULL DEFAULT NULL,
+  PRIMARY KEY (`pid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of project
+-- ----------------------------
+INSERT INTO `project` VALUES (1, ' ***大学OA', 500000);
+INSERT INTO `project` VALUES (2, '学生选课系统', 100000);
+INSERT INTO `project` VALUES (3, '讲师测评系统', 20000);
+INSERT INTO `project` VALUES (4, '线上问答系统 ', 20000);
 
 -- ----------------------------
 -- Table structure for project_record
 -- ----------------------------
 DROP TABLE IF EXISTS `project_record`;
-CREATE TABLE `project_record`
-(
-    `empno` int NOT NULL,
-    `pid`   int NOT NULL,
-    PRIMARY KEY (`empno`, `pid`) USING BTREE,
-    INDEX   `fk_project_pro`(`pid`) USING BTREE
+CREATE TABLE `project_record`  (
+  `empno` int NOT NULL,
+  `pid` int NOT NULL,
+  PRIMARY KEY (`empno`, `pid`) USING BTREE,
+  INDEX `fk_project_pro`(`pid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of project_record
+-- ----------------------------
+INSERT INTO `project_record` VALUES (7369, 1);
+INSERT INTO `project_record` VALUES (7521, 1);
+INSERT INTO `project_record` VALUES (7369, 2);
+INSERT INTO `project_record` VALUES (7499, 2);
+INSERT INTO `project_record` VALUES (7521, 2);
+INSERT INTO `project_record` VALUES (7369, 3);
+INSERT INTO `project_record` VALUES (7499, 3);
+INSERT INTO `project_record` VALUES (7521, 3);
+INSERT INTO `project_record` VALUES (7369, 4);
+INSERT INTO `project_record` VALUES (7499, 4);
 
 -- ----------------------------
 -- Procedure structure for getEmpAndDept
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `getEmpAndDept`;
-delimiter;;
-CREATE PROCEDURE `getEmpAndDept`(IN deptno int (11))
+delimiter ;;
+CREATE PROCEDURE `getEmpAndDept`(IN deptno int(11))
 BEGIN
-select *
-from emp e
-         left join dept d on (e.deptno = d.deptno)
-where e.deptno = deptno;
+select * from emp e left join dept d on (e.deptno = d.deptno) where e.deptno = deptno;
 END
 ;;
 delimiter ;
 
-SET
-FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;
