f41b9dd459382d237dfa3f35f37eb3e834fbc09a
zhanghepeng <zhanghp1221@126.com>
2023-07-21 18:37:00 +0800

    improvement:无用代码

--- lab_03_file/src/main/java/com/zhanghp/connection/YmlConfig.java
+++ lab_03_file/src/main/java/com/zhanghp/connection/YmlConfig.java
@@ -1,4 +1,4 @@
-package com.zhanghp.con;
+package com.zhanghp.connection;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
